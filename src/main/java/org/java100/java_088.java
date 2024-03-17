9a2d74a3c37c78c5e2b9b9609b2cf36098557598
Park Yugyeong <118903476+yugyeong2@users.noreply.github.com>
2024-03-17 20:21:31 +0900

    Update and rename ex100_88.java to java_088.java

--- src/main/java/org/java100/java_088.java
+++ src/main/java/org/java100/java_088.java
@@ -1,14 +1,13 @@
 package org.java100;
 import java.util.Scanner;
 
-public class ex100_88 {
+public class java_088 {
 
 	public static void main(String[] args) {
-		// ������ ��
 		//Sum = 1+2+3+4+5
 		//Sum = 1 + (1+2) + (1+2+3) ... (1+...+n)
 		Scanner scan = new Scanner(System.in);
-		System.out.println("n����:");
+		System.out.println("n:");
 		int n = scan.nextInt();
 		int sum = 0;
 		for(int i=1; i<=n; i++) {
@@ -16,7 +15,7 @@ public class ex100_88 {
 				sum = sum + j;
 			}
 		}
-		System.out.println("��������:"+sum);
+		System.out.println("수열의 합:"+sum);
 		//Sum = 1 + (1+2) + (1+2+3) ... (1+...+n)
 		int sum1 = 0;
 		int sum2 = 0;
@@ -24,7 +23,13 @@ public class ex100_88 {
 			sum1 = sum1 + i;
 			sum2 = sum2 + sum1;
 		}
-		System.out.println("��������:"+sum2);
+		System.out.println("수열의 합:"+sum2);
 	}
 
 }
+/*
+	수열의 합 S_n의 값을 출력한다.
+
+	Sum = 1+2+3+4+5
+	Sum = 1 + (1+2) + (1+2+3) ... (1+...+n)
+*/
