9809d0e44606d63d2aadc0d79932b5bbafd260b0
Park Yugyeong <118903476+yugyeong2@users.noreply.github.com>
2024-03-17 17:49:41 +0900

    Update java_074.java

--- src/main/java/org/java100/java_074.java
+++ src/main/java/org/java100/java_074.java
@@ -3,8 +3,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class ex100_74 {
-
+public class java_074 {
 	public static void main(String[] args) {
 
 		Scanner scan = new Scanner(System.in);
