c1ed3f06ef414ba22b6e45421e0f50f847d3fda4
Park Yugyeong <118903476+yugyeong2@users.noreply.github.com>
2024-03-17 17:48:14 +0900

    Update and rename ex100_86.java to java_086.java

--- src/main/java/org/java100/java_086.java
+++ src/main/java/org/java100/java_086.java
@@ -5,14 +5,6 @@ import java.util.Scanner;
 public class ex100_86 {
 
 	public static void main(String[] args) {
-
-		//�ﰢ�� ����ϱ�4 
-		//Ȧ�� n�� �Է¹���.
-		//   *
-		//  ***
-		// *****
-		// n=5�϶�, �ټ��� 3, ����ġ 3
-		//            n/2+1
 		int n = 5;
 		int midIndex = (n/2)+1;
 		for(int i=1; i<=midIndex; i++) {
@@ -26,3 +18,18 @@ public class ex100_86 {
 		}
 	}
 }
+
+/*
+	어떤 수 n을 입력받으면 다음과 같은 삼각형을 출력한다.
+	여기서 입력되는 n은 반드시 홀수이다.
+	
+	입력 예시 
+	5
+	출력 예시
+	  *
+	 ***
+	*****
+	
+	n=5일때, 줄수가 3, 별위치 3
+	즉 n/2+1
+*/
