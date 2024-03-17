18d17ab6109628fc072898c68ea35f6999e2e511
Park Yugyeong <118903476+yugyeong2@users.noreply.github.com>
2024-03-17 17:53:18 +0900

    Update java_084.java

--- src/main/java/org/java100/java_084.java
+++ src/main/java/org/java100/java_084.java
@@ -1,8 +1,7 @@
 ﻿package org.java100;
 import java.util.Scanner;
 
-public class ex100_84 {
-
+public class java_084 {
 	public static void main(String[] args) {
 
 		Scanner scan = new Scanner(System.in);
