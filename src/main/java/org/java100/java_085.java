ec16a7a5b1cfb5b1e135e0ef1ae055def85a585e
Park Yugyeong <118903476+yugyeong2@users.noreply.github.com>
2024-03-17 17:46:06 +0900

    Update and rename ex100_85.java to java_085.java

--- src/main/java/org/java100/java_085.java
+++ src/main/java/org/java100/java_085.java
@@ -8,11 +8,7 @@ public class ex100_85 {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("n?:");
 		int n = scan.nextInt();
-		//*****
-		// ****
-		//  ***
-		//   **
-		//    *
+
 		for(int i=0; i<n; i++) {
 			for(int j=0; j<i; j++) {
 				System.out.print(" ");
@@ -25,3 +21,16 @@ public class ex100_85 {
 	}
 
 }
+
+/*
+	길이 n이 입력되면 다음과 같은 역삼각형을 출력한다.
+	
+	입력 예시 
+	5
+	출력 예시
+	*****
+	****
+	 ***
+	  **
+	   *
+*/
