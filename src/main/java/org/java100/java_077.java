1962c04b4328749221958e5ff9121e87fb52cad4
Park Yugyeong <118903476+yugyeong2@users.noreply.github.com>
2024-03-17 17:33:31 +0900

    Update and rename ex100_77.java to java_077.java

--- src/main/java/org/java100/java_077.java
+++ src/main/java/org/java100/java_077.java
@@ -6,9 +6,9 @@ public class ex100_76 {
 	public static void main(String[] args) {
 
 		Scanner scan = new Scanner(System.in);
-		System.out.print("������ �Է��ϼ���>");
+		System.out.print("지수를 입력하세요>");
 		int n = scan.nextInt();
-		System.out.print("������ �Է��ϼ���>");
+		System.out.print("기수를 입력하세요>");
 		int k = scan.nextInt();
 		int num = (int)Math.pow(n, k);
 		
