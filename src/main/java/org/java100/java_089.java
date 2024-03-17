aeed06fc0ef558c640250342bc002bd0f697f92b
Park Yugyeong <118903476+yugyeong2@users.noreply.github.com>
2024-03-17 20:22:34 +0900

    Update and rename ex100_89.java to java_089.java

--- src/main/java/org/java100/java_089.java
+++ src/main/java/org/java100/java_089.java
@@ -1,15 +1,8 @@
 package org.java100;
 
-public class ex100_89 {
-
+public class java_089 {
 	public static void main(String[] args) {
-		// �� �ֻ����� ��
-		// �ֻ����� �ΰ� ������ ���� ������ ���
-		// 5
-		// 1, 4
-		// 2, 3
-		// 3, 2
-		// 4, 1
+
 		int sum = 12;
 		for(int i=1; i<=6; i++) {
 			for(int j=1; j<=6; j++) {
@@ -22,3 +15,13 @@ public class ex100_89 {
 	}
 
 }
+
+/*
+	두 주사위의 합
+	주사위를 두개 굴려서 합이 나오는 경우
+	5
+	1, 4
+	2, 3
+	3, 2
+	4, 1
+*/
